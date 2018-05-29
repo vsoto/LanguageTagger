132a31eee54186e444d541db5c4c3d650494e0f7
Victor Soto <vsoto@cs.columbia.edu>
2018-05-29 15:32:24 -0400

    First commit with code

---
+++ src/language_id/com/detectLanguage/responses/BatchDetectResponse.java
@@ -0,0 +1,5 @@
+package language_id.com.detectLanguage.responses;
+
+public class BatchDetectResponse extends Response {
+    public BatchDetectionsData data;
+}
---
+++ src/language_id/com/detectLanguage/responses/ErrorData.java
@@ -0,0 +1,6 @@
+package language_id.com.detectLanguage.responses;
+
+public class ErrorData {
+    public int code;
+    public String message;
+}
